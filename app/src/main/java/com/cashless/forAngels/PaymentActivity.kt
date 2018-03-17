6b9b6e684d72748a4f6097d740e96f0a261def1c
antonello <m3rcur14l@hotmail.com>
2018-03-17 23:08:43 +0100

    InfoResponse response integration

--- app/src/main/java/com/cashless/forAngels/PaymentActivity.kt
+++ app/src/main/java/com/cashless/forAngels/PaymentActivity.kt
@@ -1,9 +1,12 @@
 package com.cashless.forAngels
 
+import android.app.ProgressDialog
 import android.nfc.NfcAdapter
 import android.os.Bundle
 import com.cashless.forAngels.service.AngelService
+import com.cashless.forAngels.service.response.InfoResponse
 import dagger.android.support.DaggerAppCompatActivity
+import io.reactivex.android.schedulers.AndroidSchedulers
 import javax.inject.Inject
 
 
@@ -11,9 +14,15 @@ class PaymentActivity : DaggerAppCompatActivity() {
 
     @Inject lateinit var angelService: AngelService
 
+    private var progressDialog: ProgressDialog? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_payment)
+
+        progressDialog = ProgressDialog.show(this,
+                "Un momento...", "",
+                true)
 
         var alias = ""
         intent?.let {
@@ -21,6 +30,21 @@ class PaymentActivity : DaggerAppCompatActivity() {
                 alias = it.data.getQueryParameter("card")
         }
 
+        alias.isNotEmpty().also {
+            angelService.getProfileInfo(alias)
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(this::onData, this::onError)
+        }
+
     }
 
+    private fun onData(respose: InfoResponse) {
+
+
+        progressDialog?.dismiss()
+    }
+
+    private fun onError(e: Throwable) {
+        progressDialog?.dismiss()
+    }
 }
--- app/src/main/java/com/cashless/forAngels/service/response/InfoResponse.kt
+++ app/src/main/java/com/cashless/forAngels/service/response/InfoResponse.kt
@@ -1,16 +1,18 @@
 package com.cashless.forAngels.service.response
 
+import com.google.gson.annotations.SerializedName
+
 
 /**
  * Created by Antonello Fodde on 17/03/18.
  * fodde.antonello@gmail.com
  */
 data class InfoResponse(
-        val cardId: Int?,
-        val organizationId: Int?,
-        val organizationName: String?,
-        val organizationImage: String?,
-        val organizationPaymentId: String?,
-        val userId: Int?,
-        val userName: String?,
-        val userImage: String?)
+        @SerializedName("card_id") val cardId: Int?,
+        @SerializedName("org_id") val organizationId: Int?,
+        @SerializedName("org_name") val organizationName: String?,
+        @SerializedName("org_img") val organizationImage: String?,
+        @SerializedName("org_pay") val organizationPaymentId: String?,
+        @SerializedName("id_user") val userId: Int?,
+        @SerializedName("user_name") val userName: String?,
+        @SerializedName("user_img") val userImage: String?)
